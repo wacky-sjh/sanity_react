124ca9ffed12ce659cd00344a0ac9141944b0a04
wacky-sjh <sjh@wacky.kr>
2025-06-25 17:51:42 +0900

    Refact: Home 컴포넌트에서 불필요한 Fragment 제거

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,13 +7,11 @@ export default function Home() {
   const { i18n, t } = useTranslation();
   const lang = i18n.language;
   return (
-    <>
-      <Button
-        className="bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer"
-        onClick={() => navigate(`/${lang}/news`)}
-      >
-        {t("goToNewsList")}
-      </Button>
-    </>
+    <Button
+      className="bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer"
+      onClick={() => navigate(`/${lang}/news`)}
+    >
+      {t("goToNewsList")}
+    </Button>
   );
 }
